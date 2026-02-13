be9a4a7edbe0a1eba228d492978b9944d2a06da7
Côme Eyraud <come.eyraud@polymtl.ca>
2026-02-13 12:08:50 -0500

    Bumping baseliner version

--- altis/level1/pathfinder/main.cu
+++ altis/level1/pathfinder/main.cu
@@ -6,9 +6,17 @@
 #include <baseliner/StoppingCriterion.hpp>
 namespace Path {
 
-  auto runner = std::make_shared<Baseliner::Runner<Baseliner::PathfinderKernel, Baseliner::Backend::CudaBackend>>();
+  auto runner = Baseliner::Runner<Baseliner::PathfinderKernel, Baseliner::Backend::CudaBackend>()
+                    .add_stat<Baseliner::Stats::Q1>()
+                    .add_stat<Baseliner::Stats::Q3>()
+                    .add_stat<Baseliner::Stats::Median>()
+                    .add_stat<Baseliner::Stats::WithoutOutliers>()
+                    .add_stat<Baseliner::Stats::MedianAbsoluteDeviation>();
 
   Baseliner::Axe axe = {"StoppingCriterion", "max_nb_repetition", {"100", "250", "500", "1000", "2000"}};
-  Baseliner::SingleAxeBenchmark bench(runner, axe);
+  Baseliner::SingleAxeBenchmark
+      bench(std::make_shared<Baseliner::Runner<Baseliner::PathfinderKernel, Baseliner::Backend::CudaBackend>>(
+                std::move(runner)),
+            axe);
   BASELINER_REGISTER_EXECUTABLE(&bench);
 } // namespace Path
--- altis/level1/sort/main.cu
+++ altis/level1/sort/main.cu
@@ -8,7 +8,9 @@ namespace Sort {
                         .set_stopping_criterion<Baseliner::ConfidenceIntervalMedianSC>()
                         .add_stat<Baseliner::Stats::Q1>()
                         .add_stat<Baseliner::Stats::Q3>()
-                        .add_stat<Baseliner::Stats::Median>();
+                        .add_stat<Baseliner::Stats::Median>()
+                        .add_stat<Baseliner::Stats::WithoutOutliers>()
+                        .add_stat<Baseliner::Stats::MedianAbsoluteDeviation>();
   BASELINER_REGISTER_EXECUTABLE(&runner_act);
 
 } // namespace Sort
